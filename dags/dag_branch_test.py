4c1d24a4e21d91e106a36e38f1dc343e0082f622
Piotr Wąsiewicz <pwasiewi@mail.com>
2022-10-11 13:55:49 +0200

    new tests

---
+++ dags/dag_branch_test.py
@@ -0,0 +1,46 @@
+"""Example DAG demonstrating the usage of the BranchPythonOperator."""
+
+import random
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.utils.edgemodifier import Label
+from airflow.utils.trigger_rule import TriggerRule
+
+with DAG(
+    dag_id='dag_branch_test',
+    start_date=datetime(2022, 10, 1),
+    catchup=True,
+    schedule_interval="@daily",
+    tags=['branch', 'NONE_FAILED_MIN_ONE_SUCCESS'],
+) as dag:
+    run_this_first = DummyOperator(
+        task_id='run_this_first',
+    )
+
+    options = ['branch_a', 'branch_b', 'branch_c', 'branch_d']
+
+    branching = BranchPythonOperator(
+        task_id='branching',
+        python_callable=lambda: random.choice(options),
+    )
+    run_this_first >> branching
+
+    join = DummyOperator(
+        task_id='join',
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS,
+    )
+
+    for option in options:
+        t = DummyOperator(
+            task_id=option,
+        )
+
+        dummy_follow = DummyOperator(
+            task_id='follow_' + option,
+        )
+
+        # Label is optional here, but it can help identify more complex branches
+        branching >> Label(option) >> t >> dummy_follow >> join
